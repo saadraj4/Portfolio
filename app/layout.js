f092424c1852a53aab33b01389094eb6bd3db663
Saad Raja <sr201040@gmail.com>
2024-08-05 02:27:20 +0500

    Resume done

--- app/layout.js
+++ app/layout.js
@@ -11,8 +11,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Saad Raja - Software Developer",
-  description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    // description:
+    //   "This is the portfolio of Saad Raja. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
